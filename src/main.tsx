24aade0586d06b93574ba3b52802d7d1751875b9
maldonae <maldonadoeric69120@gmail.com>
2024-10-30 10:39:08 +0100

    creation des fichiers reactrouter

---
+++ src/main.tsx
@@ -0,0 +1,50 @@
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+
+// page components
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+// router creation
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <main>
+          <Home />
+        </main>
+      </>
+    ),
+  },
+  {
+    path: "/about",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <main>
+          <About />
+        </main>
+      </>
+    ),
+  },
+]);
+
+// rendering
+
+const rootElement = document.getElementById("root");
+
+if (rootElement != null) {
+  ReactDOM.createRoot(rootElement).render(
+    <RouterProvider router={router} />
+  );
+}
