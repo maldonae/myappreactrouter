3ab1d3d46fbf61ba0c8917a9a17fe5d9204c7d17
maldonae <maldonadoeric69120@gmail.com>
2024-10-30 11:22:47 +0100

    modification de App lien pour que les pages s'affichent en interne

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 import "./App.css";
 
-// page components
-
-import Home from "./pages/Home";
-import About from "./pages/About";
-
-// the App
-
 function App() {
-  const [currentLocation, setCurrentLocation] = useState("/");
-
   return (
     <>
       <nav>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
-
-        <button onClick={() => setCurrentLocation("/")} type="button">
-          Home
-        </button>
-        <button onClick={() => setCurrentLocation("/about")} type="button">
-          About
-        </button>
       </nav>
       <main>
-        {currentLocation === "/" && <Home />}
-        {currentLocation === "/about" && <About />}
+        <Outlet />
       </main>
     </>
   );
